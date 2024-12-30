d025d1d09ff8e66b09739bd874b50548a42f4afc
omurcetinim <omurcetin963@gmail.com>
2024-12-30 15:16:01 +0300

    Add files via upload

---
+++ 241230/ornek1.py
@@ -0,0 +1,4 @@
+simitin_fiyati=15
+simitin_adeti=int(input("Aldığınız Simit Miktarını Giriniz: "))
+tutar=simitin_fiyati*simitin_adeti
+print("Toplam Tutar: " , tutar)
---
+++ 241230/ornek2.py
@@ -0,0 +1,6 @@
+hiz=int(input("Hızınızı Giriniz: "))
+sure=int(input("Sürenizi Giriniz: "))
+mesafe=int(input("Gitmeniz Gereken Mesafeyi Giriniz: "))
+gittigin_mesafe=hiz*sure
+kalan_mesafe=mesafe - gittigin_mesafe
+input("Kalan Mesafeniz: " , kalan_mesafe)
